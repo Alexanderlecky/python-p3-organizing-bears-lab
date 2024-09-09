7505f01951c362b5950320d31a49b720caf953e8
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-09-10 00:08:06 +0300

    Bears

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 3, 'F', 'blonde', 'playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 7, 'M', 'black', 'aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 12, 'M', 'brown', 'calm', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 5, 'F', 'white', 'gentle', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 6, 'M', 'gray', 'grumpy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 4, 'F', 'black', 'curious', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 8, 'F', 'brown', 'shy', 1);
+
+
